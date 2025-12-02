7a30b5f0c1abb80afb7ecabe7fe99c1ac582b451
Urvashi Patil <patilurvashi50@gmail.com>
2025-12-02 21:34:04 +0530

    Fix os import for SECRET_KEY env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +28,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+DEBUG = os.environ.get("DEBUG") == "True"
+ALLOWED_HOSTS = ["*"]  # or use env if you prefer
 
 
 # Application definition
